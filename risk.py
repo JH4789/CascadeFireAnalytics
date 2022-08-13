d7fae76a80c8ce3b250d1478263166233c065ae0
Jayden <jayden.cx.huang@gmail.com>
2022-08-12 22:08:42 -0700

    BigChangesUsingDFNow

--- risk.py
+++ risk.py
@@ -1,10 +1,7 @@
-#TF + Keras
-#NEED UPDATE HERE PLEASE!!!!!!!!! UPDATE MEDIUM
-#RANDOM FOREST (ONE POSSIBLE METHOD)
-#FIX ERRORS IN WEB SCRAPE (TRY-CATCH)
 #Joint Probability Mass Distribution?, this may be promising to actually generate a risk index
 #Look into SciPy
 #New dataset? Caps downloads at 1000 records a day doing initial testing with tillamook
+#Legacy code being phased out, maybe delete some stuff at the bottom
 import timeit
 import csv
 import urllib.request
@@ -30,6 +27,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.tree import export_graphviz
 from subprocess import check_call
+import pandas as pd
 #import pydot
 """
 Note index 71 is for flame length
@@ -41,21 +39,18 @@ def main():
     maximum = sys.maxsize
     masterdata = []
     count = 0
-    with open('ODF_Fire.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ',')
-        for row in csv_reader:            
-            if count != 0:
-                newaddition = []
-                for column in row:
-                    newaddition.append(column)
-                masterdata.append(newaddition)
-            count +=1
-    
+    masterdata = pd.read_csv('ODF_Fire.csv')
+    testmaster = pd.read_csv('tillamook_total.csv')
     #All ignition time entries after 2008 go boom using the below line of code
     #flamelength, firesize,longitude,latitude,ignition,serial = filterInt(masterdata, 63,73,23,24,54,2)
+    masterdata =process_master(masterdata,52)
     
+    testmaster = testmaster.dropna(subset = ["humidity"])
+    
+    newWrapper(masterdata, '2015/5/1','2019/5/1', 51, 'Tillamook', testmaster)
     #writeFile(masterdata, '2012/2/10', '2020/9/1', 51, 'Tillamook', 'tillamook.csv')
-    bigWrapper(masterdata, '2013/5/1','2019/09/1', 51, 'Tillamook','tillamook.csv')
+    #bigWrapper(masterdata, '2015/5/1','2019/5/1', 51, 'Tillamook','tillamook.csv')
+    #print(returnDates('2018/5/1','2020/5/1'))
     stop = timeit.default_timer()
     print('Time: ', stop - start)  
 def writeFile(masterdata, start, end, citynum, cityname, filename):
@@ -71,25 +66,57 @@ def writeFile(masterdata, start, end, citynum, cityname, filename):
             data.append(row[1])
             data.append(row[2])
             writer.writerow(data)
+def newWrapper(masterdata, start, end, citynum, cityname, environmentdata):
+    ignition = masterdata[['Ign_DateTime']]
+    dates = returnDates(start, end)
+    
+    ignition[['Ign_DateTime', 'Filler']] = ignition['Ign_DateTime'].str.split(' ',1,expand=True)
+    del ignition['Filler']
+    ignition['Ign_DateTime'] = ignition['Ign_DateTime'].str.replace('/','-')
+    
+    ignition = ignition[ignition['Ign_DateTime'].isin(dates)]
+    
+    #print(ignition)
+    
+    loaded = loadData('astoria.csv',start,end)
+    
+    finalcount = []
+    environment = []
+    counter = 0
+
+    count = 0
+    print(environmentdata)
+    environmentdata = environmentdata[environmentdata.datetime.isin(dates)]
+    environmentdata = environmentdata[["humidity","windspeed","tempmax"]]
+    print(environmentdata)
+    #scraped = webScrape(start,end, cityname)
+    #countarray = countFires(ignition, loaded)
+    randomForest(environmentdata, countFires(ignition,dates))
 def bigWrapper(masterdata, start , end , citynum, cityname, filename):
-    filler,filler,filler,unit,ignition, serial,  = filterInt(masterdata,0,0,0,8,54,2,citynum)
-    ignition = processTimes(ignition, start,end)
+    #filler,filler,filler,unit,ignition, serial,  = filterInt(masterdata,0,0,0,8,54,2,citynum)
+    ignition = masterdata[['Ign_DateTime']]
+    dates = returnDates(start, end)
+    ignition[['Ign_DateTime', 'Filler']] = ignition['Ign_DateTime'].str.split(' ',1,expand=True)
+    del ignition['Filler']
+    ignition['Ign_DateTime'] = ignition['Ign_DateTime'].str.replace('/','-')
+    print(ignition)
+    ignition = ignition[ignition['Ign_DateTime'].isin(dates)]
     loaded = loadData(filename,start,end)
     print(ignition)
     finalcount = []
     environment = []
     counter = 0
+    count = 0
     #scraped = webScrape(start,end, cityname)
-    countarray = countFires(ignition, loaded)
-    
+    countarray = countFires(ignition, dates)
+    processedarray = []
     for row in loaded:
-        temp = []
-        temp.append(row[1])
-        temp.append(row[2])
-        environment.append(temp)
-    
-    for row in countarray:
-        finalcount.append(row[1])
+        if row[2] < 500:
+            temp = []
+            temp.append(row[1])
+            temp.append(row[2])
+            environment.append(temp)
+        count +=1
     """
     with open( 'astoria.csv', 'w') as filewrite:
         writer = csv.writer(filewrite)
@@ -101,13 +128,15 @@ def bigWrapper(masterdata, start , end , citynum, cityname, filename):
             data.append(row[2])
             writer.writerow(data)
     """
-    randomForest(environment, finalcount)
+    randomForest(environment, countFires(ignition,dates))
 def countFires(ignition, scraped):
     newarray = []
     for row in scraped:
         temp = []
-        temp.append(row[0])
-        count = ignition.count(row[0])
+        
+        count = len(ignition[ignition['Ign_DateTime'] == row])
+        #print(count)
+        #print(count)
         if count >= 1:
             count = True
         else:
@@ -130,37 +159,107 @@ def dataValidate(masterdata, serial, inputlist, index):
 def randomForest(environment, finalcount):
     X = np.asarray(environment)
     y = np.asarray(finalcount)
+    truecount = 0
+    falsecount = 0
+    for i in y:
+        if i == True:
+            truecount +=1
+        else:
+            falsecount +=1
+    print(truecount)
+    print(falsecount)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     #scaler = StandardScaler()
     #scaler.fit(X_train)
     #X_train = scaler.transform(X_train)
     #X_test = scaler.transform(X_test)
-    clf = RandomForestClassifier(max_depth = 10,min_samples_split = 50,n_estimators = 200,class_weight = "balanced")
+    clf = RandomForestClassifier(max_depth = 75,min_samples_split = 100,n_estimators = 100,class_weight = {0:1,1:40})
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    cv_results = cross_validate(clf, X, y, cv=2)
+    cv_results = cross_validate(clf, X, y, cv=3)
     print(cv_results)
     print(clf.decision_path(X_test))
     #clf.plot_tree(clf, features_names = X, class_names = y,filled = True)
     estimator = clf.estimators_[1]
+    """
     export_graphviz(estimator, out_file='tree.dot', 
-                feature_names = ['Temperature','Wind Speed'],
+                feature_names = ['Humidity', 'Temperature','Wind Speed'],
                 class_names = 'Fire Prediction',
                 rounded = True, proportion = False, 
                 precision = 2, filled = True)
+    """
     #check_call(['dot','-Tpng','tree.dot',' -o',' test.png'])
     count = 0
     print(y_pred)
+    falsecount = 0
+    truecount = 0
+    truetotal = 0
+
+    fig = plt.figure(figsize = plt.figaspect(0.5))
+    ax1 = fig.add_subplot(1,2,1,projection = "3d")
+    
+    maxtemp = [item[0] for item in X_test]
+    windspeed = [item[1] for item in X_test]
+    humidity = [item[2] for item in X_test]
+    count = 0
+    for i in maxtemp:
+        if y_test[count] == True:
+            ax1.scatter(i,windspeed[count],maxtemp[count],c="red")
+            truetotal +=1
+        else:
+            ax1.scatter(i,windspeed[count],maxtemp[count],c="green")
+        count +=1
+    ax1 = fig.add_subplot(1,2,2,projection = "3d")
+    count = 0
+    for i in maxtemp:
+        if y_pred[count] == True:
+            ax1.scatter(i,windspeed[count],maxtemp[count],c="red")
+            truetotal +=1
+        else:
+            ax1.scatter(i,windspeed[count],maxtemp[count],c="green")
+        count +=1
+    """
     for i in X_test:
         if y_test[count] == True:
             plt.scatter(i[0], i[1], color = "blue")
+            truetotal +=1
         if y_pred[count] == True:
-            print("Predicted")
+            #print("Predicted")
+            falsecount +=1
             plt.scatter(i[0],i[1], color = "green")
         if y_test[count] == True and y_pred[count] == True:
-            print("Hoepfully happned once")
+            #print("Hoepfully happned once")
+            truecount +=1
+            #print(X_test[count])
         count +=1
+    """
+    print(len(X_test))
+    print(falsecount)
+    print(truecount)
+    print(truetotal)
+    """
+    for i in X_train:
+        if y_train[count] == False:
+            #plt.scatter(i[0],i[1],color = "red")
+            plt.scatter(i[0],i[1],color = "blue")
+        count +=1
+    count = 0
+    for i in X_train:
+        if y_train[count] == True:
+            plt.scatter(i[0],i[1],color = "red")
+            
+        count +=1
+        """
     plt.show()
+def calculateRisk(range1, range2, environment, countdata, datapoint):
+    
+    for i in bigdata:
+        if datapoint[0] - range1 <= i[0] <= datapoint[0] + range1 and datapoint[1]-range2 <= i[1] <= datapoint[1]+range2:
+            
+            if countdata[count] == True:
+                total +=1
+        count +=1
+#Linear reg might need to be retired soon
 #Linear reg seems to be working better, further testing needed
 def linearreg(list1, list2,numeral):
 
@@ -185,7 +284,36 @@ def scale_data(inputdata):
     for row in inputdata:
         newarray.append(row*100)
     return newarray
-#Have to add range here :(
+def returnDates(start,end):
+    starting = start.split('/')
+    ending = end.split('/')
+    dates = mdates.num2date(mdates.drange(DT.datetime(int(starting[0]), int(starting[1]), int(starting[2])),
+                                          DT.datetime(int(ending[0]), int(ending[1]), int(ending[2])),
+                                      DT.timedelta(days=1)))
+
+    newdates = []
+    tempdates = []
+
+    for row in dates:
+        newarr = []
+        newarr.append(row.year)
+        newarr.append(row.month)
+        newarr.append(row.day)
+        tempdates.append(newarr)
+    for row in tempdates:
+        temp = []
+        tempyear = row[0]
+        tempmonth = row[1]
+        tempday = row[2]
+        if tempmonth < 10:
+            tempmonth = "0"+str(tempmonth)
+        if tempday < 10:
+            tempday = "0" + str(tempday)
+            datestr = str(tempyear) + "-" + str(tempmonth) + "-" + str(tempday)
+        else:
+            datestr = str(tempyear) + "-" + str(tempmonth) + "-" + str(tempday)
+        newdates.append(datestr)
+    return newdates
 def loadData(filename,start,end):
     newarray = []
     count = 0
@@ -268,19 +396,6 @@ def perceptron(newarray, list2):
     y_pred = ppn.predict(X_test)
     newarr = []
     #print(ppn.score(X_test,y_test))
-    print(ppn.predict([[60.8, 7.48]]))
-def getLongitude(masterdata):
-    newarray = []
-    for row in masterdata:
-        if row[23] != 'Longitude' and row[23] != '':
-            newarray.append(float(row[23]))
-    return newarray
-def getLatitude(masterdata):
-    newarray = []
-    for row in masterdata:
-        if row[24] != 'Latitude' and row[24] != '':
-            newarray.append(float(row[24]))
-    return newarray
 def findMin(array):
     final = 100000000
     for number in array:
@@ -350,27 +465,11 @@ def windSpeed(page_html):
     mean_temp = page_html[temp_marker:new_temp_marker]
     mean_temp = mean_temp.split('>')
     return mean_temp[1]
-def filterInt(masterdata, index1, index2,index3,index4,index5,index6,locationnum):
-    newarray1 = []
-    newarray2 = []
-    newarray3 = []
-    newarray4 = []
-    newarray5 = []
-    newarray6 = []
-    for row in masterdata:
-        if row[index1] != '' and row[index2] != '' and row[index3] != '' and int(row[index4]) == locationnum and row[index5] != '' and row[index6] != '':
-            newarray1.append(float(row[index1]))
-            newarray2.append(float(row[index2]))
-            newarray3.append(float(row[index3]))
-            newarray4.append(float(row[index4]))
-            newarray5.append(row[index5])
-            newarray6.append(row[index6])
-        
-    return newarray1 , newarray2, newarray3, newarray4, newarray5,newarray6
-def graphRange(masterdata, minsize, maxsize, inputcolor):
-    acres_long = filterSize(masterdata, minsize, maxsize, 23)
-    acres_lat = filterSize(masterdata, minsize, maxsize, 24)
-    plt.scatter(acres_long, acres_lat, color = inputcolor)    
+def process_master(masterdata,locationnum):
+    masterdata = masterdata.dropna(subset = ["Ign_DateTime","Current_District", "Current_Unit"])
+    print(masterdata["Current_District"])
+    masterdata = masterdata[masterdata.Current_District == locationnum]
+    return masterdata
 def processTimes(inputlist,range1, range2):
     newlist = []
     starting = range1.split('/')
